7994ae731a57b8bfc8eb48349eceb81bc1a93700
yinxunyang <yinxunyang@bestvike.com>
2019-05-16 11:20:14 +0800

    添加redisDemo

---
+++ src/test/java/com/redisdemo/RedisdemoApplicationTests.java
@@ -0,0 +1,28 @@
+package com.redisdemo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RedisdemoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+	@Autowired
+	StringRedisTemplate stringRedisTemplate;
+	@Test
+	public void test()  {
+
+		// 保存字符串
+		stringRedisTemplate.opsForValue().set("aaa", "111");
+
+	}
+
+}
