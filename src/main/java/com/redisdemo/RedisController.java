247c5493ba7cc64c770ca2ddf3ff13cc5e9cc19d
yinxunyang <yinxunyang@bestvike.com>
2019-05-21 14:03:48 +0800

    redis事务

--- src/main/java/com/redisdemo/RedisController.java
+++ src/main/java/com/redisdemo/RedisController.java
@@ -2,21 +2,14 @@ package com.redisdemo;
 
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.redis.connection.RedisZSetCommands;
-import org.springframework.data.redis.core.BoundZSetOperations;
-import org.springframework.data.redis.core.DefaultTypedTuple;
+import org.springframework.data.redis.core.RedisOperations;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.StringRedisTemplate;
-import org.springframework.data.redis.core.ZSetOperations;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import java.awt.font.NumericShaper;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Map;
-import java.util.Set;
+import java.util.List;
 
 @Controller
 @RequestMapping("/redis")
@@ -28,46 +21,24 @@ public class RedisController {
     private StringRedisTemplate stringRedisTemplate;
 
 
-    @RequestMapping("/list")
+    @RequestMapping("/testwatch")
     @ResponseBody
-    public Map<String, Object> testList() {
-        Set<ZSetOperations.TypedTuple<String>> typedTupleSet = new HashSet<>();
-        for (int i = 1; i <= 9; i++) {
-            // 分数
-            double score = i * 0.1;
-            // 创建一个TypedTuple对象，存入值和分数
-            ZSetOperations.TypedTuple<String> typedTuple = new DefaultTypedTuple<String>("value" + i, score);
-            typedTupleSet.add(typedTuple);
-        }
-        // 往有序集合插入元素
-        stringRedisTemplate.opsForZSet().add("zset1", typedTupleSet);
-        // 绑定zset1有序集合操作
-        BoundZSetOperations<String, String> zsetOps = stringRedisTemplate.boundZSetOps("zset1");
-        // 增加一个元素
-        zsetOps.add("value10", 0.26);
-        Set<String> setRange = zsetOps.range(1, 6);
-        // 按分数排序获取有序集合
-        Set<String> setScore = zsetOps.rangeByScore(0.2, 0.6);
-        // 定义值范围
-        RedisZSetCommands.Range range = new RedisZSetCommands.Range();
-        range.gt("value3");// 大于value3
-        // range.gte("value3");// 大于等于value3
-        // range.lt("value8");// 小于value8
-        range.lte("value8");// 小于等于value8
-        // 按值排序，请注意这个排序是按字符串排序
-        Set<String> setLex = zsetOps.rangeByLex(range);
-        // 删除元素
-        zsetOps.remove("value9", "value2");
-        // 求分数
-        Double score = zsetOps.score("value8");
-        // 在下标区间下，按分数排序，同时返回value和score
-        Set<ZSetOperations.TypedTuple<String>> rangeSet = zsetOps.rangeWithScores(1, 6);
-        // 在分数区间下，按分数排序，同时返回value和score
-        Set<ZSetOperations.TypedTuple<String>> scoreSet = zsetOps.rangeByScoreWithScores(1, 6);
-        // 按从大到小排序
-        Set<String> reverseSet = zsetOps.reverseRange(2, 8);
-        Map<String, Object> map = new HashMap<String, Object>();
-        map.put("success", true);
-        return map;
+    public void testwatch() {
+        redisTemplate.opsForValue().set("k1", "v1");
+        List list = (List) redisTemplate.execute(
+                (RedisOperations redisop) -> {
+                    // 设置要监控的key值
+                    redisop.watch("k1");
+                    // 开启事务，在exec执行前，全部都只是进入队列
+                    redisop.multi();
+                    redisop.opsForValue().set("k2", "v2");
+                    // 因为k1是字符串，对字符串加一，显然是不能进行的
+                    redisop.opsForValue().increment("k1", 1);
+                    // 获取值将为空，因为redis只是把命令放入队列
+                    Object value = redisop.opsForValue().get("k2");
+                    return redisop.exec();
+                }
+        );
+        System.out.println(list);
     }
 }
